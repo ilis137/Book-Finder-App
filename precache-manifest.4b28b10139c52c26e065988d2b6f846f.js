ebfd5e76cbc3543c307f2bc6d3a7687b96a8ca70
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-03-03 17:28:17 +0530

    Updates

---
+++ precache-manifest.4b28b10139c52c26e065988d2b6f846f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "548f1359c479433fd677",
+    "url": "/book-finder-app/static/js/runtime~main.548f1359.js"
+  },
+  {
+    "revision": "6f65317dccbd24057ca9",
+    "url": "/book-finder-app/static/js/main.6f65317d.chunk.js"
+  },
+  {
+    "revision": "080e66aa5e3a2ddd78d0",
+    "url": "/book-finder-app/static/js/2.080e66aa.chunk.js"
+  },
+  {
+    "revision": "6f65317dccbd24057ca9",
+    "url": "/book-finder-app/static/css/main.d4ba5bb2.chunk.css"
+  },
+  {
+    "revision": "30db158b0d3b902d2cd64f2f2a84e485",
+    "url": "/book-finder-app/index.html"
+  }
+];
